786b996fcac867a9d9d0165a98d870cc11a8c966
amirreza azemati <amirreza.azemati@gmail.com>
2025-12-08 17:24:40 +0100

    vite & ts config + navbar

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),],
+    resolve: {
+      alias: {
+        '#components' : resolve(dirname(fileURLToPath(import.meta.url)), 'src/components'),
+        '#constans' : resolve(dirname(fileURLToPath(import.meta.url)), 'src/constans'),
+        '#store' : resolve(dirname(fileURLToPath(import.meta.url)), 'src/store'),
+        '#hoc' : resolve(dirname(fileURLToPath(import.meta.url)), 'src/hoc'),
+        '#windows' : resolve(dirname(fileURLToPath(import.meta.url)), 'src/windows'),
+      }
+    }
 })
