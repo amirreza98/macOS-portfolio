9145d5452c120f3cf72a31fbc131c2d2fe0d92e9
amirreza azemati <amirreza.azemati@gmail.com>
2025-12-11 20:58:07 +0100

    Implement Text Reader Text in windows

--- src/windows/index.js
+++ src/windows/index.js
@@ -4,5 +4,6 @@ import Instagram from "./Instagram"
 import ChatGPT from "./ChatGPT"
 import Resume from "./Resume"
 import Finder from "./Finder"
+import Text from "./Text"
 
-export {Terminal, Safari, Instagram, ChatGPT, Resume, Finder};
+export {Terminal, Safari, Instagram, ChatGPT, Resume, Finder, Text};
